e5dcbc8751c85433d17c2596123bad0e51473603
Alex Rice <alexrice999@hotmail.co.uk>
2019-08-23 23:42:16 +0100

    Add unicode-font option

--- modules/font.nix
+++ modules/font.nix
@@ -3,20 +3,33 @@
 with lib;
 
 let
-  cfg = config.appearance.font;
+  cfg = config.appearance.fonts;
 in
 {
-  options.appearance.font = mkOption{
-    type = with types; nullOr str;
-    default = null;
-    description = ''
-      Font string or null to not set
-    '';
+  options.appearance.fonts = {
+    font = mkOption {
+      type = with types; nullOr str;
+      default = null;
+      description = ''
+        Font string or null to not set
+      '';
+    };
+
+    unicode-font = mkOption {
+      type = with types; nullOr str;
+      default = null;
+      description = ''
+        Unicode font string or null to not set
+      '';
+    };
   };
 
-  config = mkIf (cfg != null) {
-    init-el.preamble = ''
-      (add-to-list 'default-frame-alist '(font . "${cfg}"))
-    '';
-  };
+  config.init-el.preamble = mkMerge [
+    (mkIf (cfg.font != null) ''
+      (set-frame-font "${cfg.font}" nil t)
+    '')
+    (mkIf (cfg.unicode-font != null) ''
+      (set-fontset-font t 'unicode "${cfg.unicode-font}" nil 'prepend)
+    '')
+  ];
 }
