4cd9978859ccf97e8e02fe86bca9a8412b0efa1f
Alex Rice <alexrice999@hotmail.co.uk>
2019-08-26 12:55:27 +0100

    electric ret in smartparens, see smartparens#80

--- modules/packages/smartparens.nix
+++ modules/packages/smartparens.nix
@@ -15,8 +15,16 @@ with lib;
       sp-highlight-wrap-overlay = mkDefault false;
       sp-highlight-wrap-tag-overlay = mkDefault false;
     };
-    config = mkDefault [
-      "(require 'smartparens-config)"
+    config = mkDefault [ ''
+      (require 'smartparens-config)
+      (defun newline-indent (&rest _ignored)
+        "Insert an extra newline after point, and reindent."
+        (newline)
+        (indent-according-to-mode)
+        (forward-line -1)
+        (indent-according-to-mode))
+      (sp-local-pair 'prog-mode "{" nil :post-handlers '((newline-indent "RET")))
+    ''
     ];
   };
 }
