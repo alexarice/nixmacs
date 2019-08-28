39d048ccfd91b5c17be48f73f0d52d445decdaf3
Alex Rice <alexrice999@hotmail.co.uk>
2019-08-28 01:29:52 +0100

    Make diminish and delight only install if necessary

--- modules/bootstrap.nix
+++ modules/bootstrap.nix
@@ -1,15 +1,23 @@
-{ config, epkgs, ... }:
+{ config, epkgs, lib, ... }:
 
+with lib;
 {
-  config = {
-    rawPackageList = with epkgs.melpaStablePackages; [ use-package diminish ] ++ [ epkgs.elpaPackages.delight ];
+  config = let
+    packages = attrValues config.package;
+    useDiminish = any (x: x.diminish != null) packages;
+    useDelight = any (x: x.delight != null) packages;
+  in {
+    rawPackageList =
+      singleton epkgs.use-package
+      ++ optional useDiminish epkgs.diminish
+      ++ optional useDelight epkgs.delight;
 
-    init-el.preamble = ''
+    init-el.preamble = mkMerge [''
       (eval-when-compile
         (require 'use-package))
-
-      (require 'diminish)
-      (require 'delight)
-    '';
+      ''
+      (mkIf useDiminish "(require 'diminish)")
+      (mkIf useDelight "(require 'delight)")
+    ];
   };
 }
