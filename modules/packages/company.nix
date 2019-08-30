de80b46f2f03f0ba58929913c061eb4b3b86604c
Alex Rice <alexrice999@hotmail.co.uk>
2019-08-30 21:01:49 +0100

    Fix typo

--- modules/packages/company.nix
+++ modules/packages/company.nix
@@ -7,7 +7,7 @@ with lib;
     custom = {
       company-idle-delay = mkDefault 0.1;
       company-minimum-prefix-length = mkDefault 2;
-      company-seletcion-wrap-around = mkDefault true;
+      company-selection-wrap-around = mkDefault true;
     };
     diminish = "company-mode";
   };
