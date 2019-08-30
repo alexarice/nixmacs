01b50fdf686d67aa31cff82c149e5280b38f5a75
Alex Rice <alexrice999@hotmail.co.uk>
2019-08-30 11:43:24 +0100

    Add missing default

--- modules/packages/irony.nix
+++ modules/packages/irony.nix
@@ -7,7 +7,7 @@ with lib;
   config.package.irony = {
     external-packages = mkDefault [ pkgs.irony-server ];
     hook = mkDefault "((c++-mode c-mode) . irony-mode)";
-    custom.irony-cdb-compilation-databases = "'(irony-cdb-libclang irony-cdb-clang-complete)";
+    custom.irony-cdb-compilation-databases = mkDefault "'(irony-cdb-libclang irony-cdb-clang-complete)";
     config = mkDefault ''
       (add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
     '';
