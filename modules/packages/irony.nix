39590bbf748d0ffec9205bb4eccee5d4bba60ddb
Alex Rice <alexrice999@hotmail.co.uk>
2019-09-01 15:41:48 +0100

    Remove poor irony variable

--- modules/packages/irony.nix
+++ modules/packages/irony.nix
@@ -7,7 +7,6 @@ with lib;
     external-packages = mkDefault [ pkgs.irony-server ];
     use-package = {
       hook = mkDefault "((c++-mode c-mode) . irony-mode)";
-      custom.irony-cdb-compilation-databases = mkDefault "'(irony-cdb-libclang irony-cdb-clang-complete)";
       config = mkDefault ''
         (require 'irony-cdb)
         (add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
