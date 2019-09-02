a11fd8c354df1f8585bc43b28bfe22125a5c91b9
Alex Rice <alexrice999@hotmail.co.uk>
2019-09-02 19:08:44 +0100

    Cleanup build file

--- nixmacs/default.nix
+++ nixmacs/default.nix
@@ -4,6 +4,8 @@ let
   binpath = lib.makeBinPath externalPackageList;
   initElDrv = (writeText "init.el" initEl);
 
+  emacsExecutable = "${emacsPackage}/bin/emacs";
+
   pname = "nixmacs";
   version = "0.1.0";
 in
@@ -12,15 +14,14 @@ runCommand "${pname}-${version}" {
   inherit pname version;
   nativeBuildInputs = [ makeWrapper ];
 } ''
-  mkdir -p $out/share/doc
-  cp ${initElDrv} $out/init.el
-  ${emacsPackage}/bin/emacs -batch -f batch-byte-compile $out/init.el
-  cp -r ${docs.json}/* $out/share/doc
+  install -D ${initElDrv} $out/init.el
+  ${emacsExecutable} -batch -f batch-byte-compile $out/init.el
+  install -D ${docs.json}/* -t $out/share/doc
   cp -r ${docs.html}/* $out
   cp -r ${docs.manPages}/* $out
   makeWrapper ${runtimeShell} $out/shell \
     --prefix PATH : ${binpath}
-  makeWrapper ${emacsPackage}/bin/emacs $out/bin/nixmacs \
+  makeWrapper ${emacsExecutable} $out/bin/nixmacs \
     --add-flags "-q -l $out/init.elc" \
     --set INITEL $out/init.el \
     --prefix PATH : ${binpath} \
