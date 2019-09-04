880693b9f7cb14c44cefed70bc5e8f5e19466401
Alex Rice <alexrice999@hotmail.co.uk>
2019-09-04 18:05:36 +0100

    Move yasnippet settings to package

--- modules/layers/completion.nix
+++ modules/layers/completion.nix
@@ -8,67 +8,21 @@ in
 {
 
   options.layers.completion = {
-    enable = mkEnableOption "company Layer";
-
-    yas-expand-key = mkOption {
-      type = types.str;
-      default = "TAB";
-      description = ''
-        Binding for yas-expand
-      '';
-    };
-
-    yas-snippet-dirs = mkOption {
-      type = types.str;
-      default = "(list (concat user-emacs-directory \"snippets/\"))";
-      description = ''
-        yasnippet snippet directory
-      '';
-    };
-
-    default-backends = mkOption {
-      type = with types; listOf str;
-      default = [
-        "company-dabbrev"
-        "company-bbdb"
-        "company-oddmuse"
-        "company-eclim"
-        "company-semantic"
-        "company-xcode"
-        "company-cmake"
-        "company-capf"
-        "company-files"
-        "(company-dabbrev-code company-gtags company-etags company-keywords)"
-      ];
-      description = ''
-        Default company backends to be used in every buffer
-      '';
-    };
+    enable = mkEnableOption "completion layer";
   };
 
   config = {
     package = {
       yasnippet = {
         enable = true;
-        use-package = {
-          bind."yas-minor-mode-map" =
-            if cfg.yas-expand-key == "TAB" then {} else {
-              "TAB" = "nil";
-              "<tab>" = "nil";
-              "${cfg.yas-expand-key}" = "yas-expand";
-            };
-          custom.yas-snippet-dirs = "${cfg.yas-snippet-dirs}";
-        };
       };
 
       auto-yasnippet = {
         enable = true;
-        use-package.bind."${cfg.yas-expand-key}" = "aya-expand";
       };
 
       company = {
         enable = true;
-
       };
     };
   };
--- modules/packages/yasnippet.nix
+++ modules/packages/yasnippet.nix
@@ -2,10 +2,24 @@
 
 with lib;
 
+let
+  cfg = config.yasnippet.settings;
+in
 {
-  package.yasnippet.use-package = {
+  options.yasnippet.settings = {
+    yas-snippet-dirs = mkOption {
+      type = types.str;
+      default = "(list (concat user-emacs-directory \"snippets/\"))";
+      description = ''
+        yasnippet snippet directory
+      '';
+    };
+  };
+
+  config.package.yasnippet.use-package = {
     commands = mkDefault [ "yas-global-mode" "yas-minor-mode" ];
     hook = mkDefault "(prog-mode . yas-minor-mode)";
     diminish = "yas-minor-mode";
+    custom.yas-snippet-dirs = "${cfg.yas-snippet-dirs}";
   };
 }
