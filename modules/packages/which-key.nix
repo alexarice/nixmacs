33a00378cc387d6b7f62c32d6ab7b7a9292edcb5
Alex Rice <alexrice999@hotmail.co.uk>
2019-09-05 11:11:45 +0100

    Add which-key

---
+++ modules/packages/which-key.nix
@@ -0,0 +1,35 @@
+{ config, lib, ... }:
+
+with lib;
+
+let
+  cfg = config.package.which-key.settings;
+  setup-string = i: "(which-key-setup-${location i})";
+  location = i: if i == "minibuffer" then i else "side-window-${i}";
+in
+{
+  options.package.which-key.settings = {
+    setup = mkOption {
+      type = with types; nullOr (enum [
+        "bottom"
+        "right"
+        "right-bottom"
+        "minibuffer"
+      ]);
+      default = "bottom";
+      example = null;
+      description = ''
+        Activate which-key default config.
+      '';
+    };
+  };
+
+  config.package.which-key = {
+    use-package = {
+      config = mkDefault ''
+        ${if cfg.setup != null then setup-string cfg.setup else ""}
+        (which-key-mode)
+      '';
+    };
+  };
+}
