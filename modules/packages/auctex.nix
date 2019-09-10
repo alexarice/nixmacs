1ec1bccb6d586227486ead9d793fafe4f02ea363
Alex Rice <alexrice999@hotmail.co.uk>
2019-09-10 12:20:44 +0100

    Add tex-view-program-selection customisation

--- modules/packages/auctex.nix
+++ modules/packages/auctex.nix
@@ -2,11 +2,26 @@
 
 with lib;
 
+let
+  cfg = config.package.tex.settings;
+in
 {
-  options.latex-hooks = mkOption {
-    type = types.str;
-    readOnly = true;
-    visible = false;
+  options = {
+    latex-hooks = mkOption {
+      type = types.str;
+      readOnly = true;
+      visible = false;
+    };
+    package.tex.settings.TeX-view-program-selection = mkOption {
+      type = with types; attrsOf str;
+      default = {};
+      example = {
+        output-pdf = "zathura";
+      };
+      description = ''
+        Builds the TeX-view-program-selection variable.
+      '';
+    };
   };
 
   config = {
@@ -20,6 +35,14 @@ with lib;
     package.tex = {
       package = mkDefault epkgs.elpaPackages.auctex;
 
+      settings.TeX-view-program-selection = {
+        "(output-dvi has-no-display-manager)" = mkDefault "dvi2tty";
+        "(output-dvi style-pstricks)" = mkDefault "dvips and gv";
+        output-dvi = mkDefault "xdvi";
+        output-pdf = mkDefault "Evince";
+        output-html = mkDefault "xgd-open";
+      };
+
       use-package = {
         defer = mkDefault true;
         mode = ''("\\.tex\\'" . TeX-latex-mode)'';
@@ -29,6 +52,9 @@ with lib;
           TeX-parse-self = mkDefault true;
           TeX-syntactic-comment = mkDefault true;
           TeX-source-cerrelate-start-server = mkDefault true;
+          TeX-view-program-selection = mkDefault ''
+            '(${concatStringsSep "\n" (mapAttrsToList (n: v: "(${n} \"${v}\")") cfg.TeX-view-program-selection)})
+          '';
           LaTeX-fill-break-at-separators = mkDefault false;
         };
       };
