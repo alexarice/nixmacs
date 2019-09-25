40d3c0927b65dc8d9a790ad0ea9bae2b21b039ac
Alex Rice <alexrice999@hotmail.co.uk>
2019-09-25 23:10:55 +0100

    Add org mode agenda files

--- modules/layers/org.nix
+++ modules/layers/org.nix
@@ -3,12 +3,29 @@
 with lib;
 
 let
-  cfg = config.layers.org.enable;
+  cfg = config.layers.org;
 in
 {
-  options.layers.org.enable = mkEnableOption "org layer";
+  options.layers.org = {
+    enable = mkEnableOption "org layer";
 
-  config = mkIf cfg {
+    agenda-files = mkOption {
+      type = with types; either path (listOf path);
+      default = [];
+      description = ''
+        List of paths to add to org-agenda-files or a file containing
+        a list of files within it.
+      '';
+    };
+  };
+
+  config = mkIf cfg.enable {
     package.org-bullets.enable = mkDefault true;
+
+    settings.global-variables.org-agenda-files = if isList cfg.agenda-files then ''
+      '(${concatStringsSep " " (map (x: ''"${toString x}"'') cfg.agenda-files)})
+    '' else ''
+      "${toString cfg.agenda-files}"
+    '';
   };
 }
