85b5b5ff68e2312e563fb7ecff1cb29eafc046e1
Alex Rice <alexrice999@hotmail.co.uk>
2019-09-29 21:33:28 +0100

    fix mu4e path

--- modules/packages/mu4e.nix
+++ modules/packages/mu4e.nix
@@ -6,7 +6,7 @@ with lib;
   package.mu4e = {
     package = mkDefault [];
     use-package.init = mkDefault ''
-      (add-to-list 'load-path ${pkgs.mu}/share/site-lisp)
+      (add-to-list 'load-path "${pkgs.mu}/share/site-lisp")
     '';
   };
 }
