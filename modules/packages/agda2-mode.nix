5cab49e8e33fd1ca36a23405ce42d7c730359b26
Alex Rice <alexrice999@hotmail.co.uk>
2019-10-21 08:57:31 +0100

    Make agda not use which-key-mode

--- modules/packages/agda2-mode.nix
+++ modules/packages/agda2-mode.nix
@@ -1,11 +1,14 @@
-{ lib, ... }:
+{ config, lib, ... }:
 
 with lib;
 
 {
   package.agda2-mode = {
     use-package = {
-      mode = ''"\\.l?agda\\'"'';
+      mode = mkDefault ''"\\.l?agda\\'"'';
+      config = mkIf (config.package.which-key.enable) (mkDefault ''
+        (which-key-mode)
+      ''); # which-key and agda2 don't seem to play nice
     };
   };
 }
