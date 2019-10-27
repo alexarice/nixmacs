77dd817c91cfb8cec836eeba81f8a74bd684699c
Alex Rice <alexrice999@hotmail.co.uk>
2019-10-27 18:18:18 +0000

    Turn off which-key when using agda

--- modules/packages/agda2-mode.nix
+++ modules/packages/agda2-mode.nix
@@ -6,8 +6,8 @@ with lib;
   package.agda2-mode = {
     use-package = {
       mode = mkDefault ''"\\.l?agda\\'"'';
-      config = mkIf (config.package.which-key.enable) (mkDefault ''
-        (which-key-mode)
+      init = mkIf (config.package.which-key.enable) (mkDefault ''
+        (add-hook 'agda2-mode-hook '(lambda () (which-key-mode 0)) :local)
       ''); # which-key and agda2 don't seem to play nice
     };
   };
