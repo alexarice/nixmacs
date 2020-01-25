6f6e49125c37b512353896f30af5eb25ee076d8f
Alex Rice <alexrice999@hotmail.co.uk>
2020-01-25 16:28:26 +0000

    Add reftex mode to latex

--- modules/packages/auctex.nix
+++ modules/packages/auctex.nix
@@ -30,6 +30,7 @@ in
       (add-hook 'LaTeX-mode-hook 'TeX-source-correlate-mode)
       (add-hook 'LaTeX-mode-hook 'TeX-PDF-mode)
       (add-hook 'LaTeX-mode-hook 'flyspell-mode)
+      (add-hook 'LaTeX-mode-hook 'reftex-mode)
     '';
 
     package.tex = {
